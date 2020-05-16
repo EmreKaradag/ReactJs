c66e8da5c91f271386ce96f7ec5dac2a67418082
Emre <50354587+AliEmreKARADAG@users.noreply.github.com>
2020-05-17 02:43:46 +0300

    react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import AddUser from "./components/AddUser";
+import Users from "./components/Users";
+import User from "./components/User";
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ /* state = {
+    users : []
+  };*/
+
+ constructor(props){
+    super(props);
+
+    this.state = {
+      users : [
+        {
+          id : 1,
+          name : "Emre Karadag",
+          email : "emre@gmail.com"
+        },
+        {
+          id : 2,
+          name : "Berfin Karadag",
+          email : "berfin@gmail.com"
+        },
+        {
+          id : 3,
+          name : "Simba Karadag",
+          email : "simba@gmail.com"
+        }
+      ]
+    };
+    this.deleteUser = this.deleteUser.bind(this);
+    this.addUser = this.addUser.bind(this);
+  }
+  addUser(newUser){
+   let updatedUsers = this.state.users;
+
+   updatedUsers.push(newUser);
+
+   this.setState({
+     users : updatedUsers
+   })
+  }
+
+  deleteUser(id) {
+    let updatedUsers = this.state.users;
+    updatedUsers = updatedUsers.filter(user => user.id !== id);
+    // state direct ımmutable
+    this.setState({
+      users : updatedUsers
+    })
+  }
+
+  render() {
+    return (
+        <div className="container">
+          <h4>User App</h4>
+          <hr/>
+        <AddUser addUser = {this.addUser}/>
+          <hr/>
+          <Users deleteUser={this.deleteUser}  users={this.state.users}/>
+        </div>
+    );
+  }
 }
 
 export default App;
